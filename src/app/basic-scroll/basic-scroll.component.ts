6d86f4ff1dea32eef70404b775bf2e8411472c86
wildVeli <33237469+wildVeli@users.noreply.github.com>
2019-07-03 21:15:31 +0200

    First CompaniesService GET

--- src/app/basic-scroll/basic-scroll.component.ts
+++ src/app/basic-scroll/basic-scroll.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CompaniesService } from '../companies.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-basic-scroll',
@@ -21,17 +22,18 @@ export class BasicScrollComponent implements OnInit {
       });      
     }
   
-  getCompanies():void {
-    this.companiesService.getCompanies().subscribe(companies => this.companies = companies)
+  getCompanies()  {
+    this.companiesService.getCompanies().subscribe(companies => this.companies = companies);
   }
 
   getCountCompanies():void {
-    this.companiesService.getCountCompanies().subscribe(totalCompanies => this.totalCompanies = totalCompanies)
+    this.companiesService.getCountCompanies().subscribe(totalCompanies => this.totalCompanies = totalCompanies);
   }
   
   
   ngOnInit() {
     this.getCountCompanies();
+    this.getCompanies();
   }
 
 }
--- src/app/companies.service.ts
+++ src/app/companies.service.ts
@@ -10,16 +10,17 @@ export class CompaniesService {
   /*test with json placeholder 'http://localhost:8080/CompaniesService2/webresources/'
   specified in the proxy.conf.json to auth the CROSS security*/
   private apiUrl = '/CompaniesService2/webresources/'; 
-
+ 
 constructor(
   private http: HttpClient) { }
 
     /** GET heroes from the server */
   getCompanies () {
-    return this.http.get(this.apiUrl);
+    return this.http.get(this.apiUrl+'entity.companies');
+    console.log("Comes here 2");
   }
 
   getCountCompanies(){
-    return this.http.get(this.apiUrl+'entity.companies/count')
+    return this.http.get(this.apiUrl+'entity.companies/count');
   }
 }
